759d2a5d35b490024aa2cd0f59ac2a3a45d16fdd
Luca Nungässer <lucanungasser@yahoo.com>
2025-08-03 13:51:01 +0200

    FIX: Statusname in Ideas

--- routers/idea_router.py
+++ routers/idea_router.py
@@ -123,9 +123,11 @@ def get_ideas(current_user: Annotated[schemas.User, Depends(auth.get_current_act
     for x, id in enumerate(ids):
         idea = session.get(Idea, id)
         status = session.get(Idea_Status, idea.status_id)
+        images = idea.images
+        idea = json.loads(idea.model_dump_json())
         idea["status_name"] = status.name
-        allIdeas.append(json.loads(idea.model_dump_json()))
-        allIdeas[x]["images"] = idea.images
+        allIdeas.append(idea)
+        allIdeas[x]["images"] = images
 
     return allIdeas
 
